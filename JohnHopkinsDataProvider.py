01f32f66c1e0cc336b3ebfc21bbf9dbef48390d9
Simone Bologna <smnbologna@gmail.com>
2020-03-28 11:10:51 +0000

    Packaged international data retrieve

---
+++ JohnHopkinsDataProvider.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def getJohnHopkinsCOVIDData(countries=None):
+  df_confirmed = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+  if countries is not None: df_confirmed = df_confirmed[df_confirmed["Country/Region"].isin(countries)]
+  df_confirmed_skimmed = df_confirmed.drop(["Lat", "Long"], 1)
+  df_confirmed_rearranged = pd.melt(df_confirmed_skimmed, id_vars=["Province/State", "Country/Region"], var_name="date", value_name="confirmed")
+
+  df_recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+  if countries is not None: df_recovered = df_recovered[df_recovered["Country/Region"].isin(countries)]
+  df_recovered_skimmed = df_recovered.drop(["Lat", "Long"], 1)
+  df_recovered_rearranged = pd.melt(df_recovered_skimmed, id_vars=["Province/State", "Country/Region"], var_name="date", value_name="recovered")
+
+  df_deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+  if countries is not None: df_deaths = df_deaths[df_deaths["Country/Region"].isin(countries)]
+  df_deaths_skimmed = df_deaths.drop(["Lat", "Long"], 1)
+  df_deaths_rearranged = pd.melt(df_deaths_skimmed, id_vars=["Province/State", "Country/Region"], var_name="date", value_name="deaths")
+
+  df_merged = df_confirmed_rearranged.merge(df_deaths_rearranged, how="outer").merge(df_recovered_rearranged, how="outer")
+  df_merged = df_merged.groupby(["date", "Country/Region"]).sum().reset_index()
+  df_merged["active"] = df_merged["confirmed"] - df_merged["recovered"] - df_merged["deaths"]
+  df_merged["date"] = pd.to_datetime(df_merged["date"])
+
+  return df_merged
--- plotCountryComparison.py
+++ plotCountryComparison.py
@@ -5,7 +5,7 @@ import dateutil
 import numpy as np
 import os
 import matplotlib.dates as mdates
-
+from JohnHopkinsDataProvider import getJohnHopkinsCOVIDData
 
 
 # Ensure a major tick for each week using (interval=1) 
@@ -15,25 +15,7 @@ countries = ["China", "Italy", "United Kingdom"]
 
 categories = ["active", "confirmed", "recovered"]
 
-df_confirmed = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
-df_confirmed = df_confirmed[df_confirmed["Country/Region"].isin(countries)]
-df_confirmed_skimmed = df_confirmed.drop(["Lat", "Long"], 1)
-df_confirmed_rearranged = pd.melt(df_confirmed_skimmed, id_vars=["Province/State", "Country/Region"], var_name="date", value_name="confirmed")
-
-df_recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
-df_recovered = df_recovered[df_recovered["Country/Region"].isin(countries)]
-df_recovered_skimmed = df_recovered.drop(["Lat", "Long"], 1)
-df_recovered_rearranged = pd.melt(df_recovered_skimmed, id_vars=["Province/State", "Country/Region"], var_name="date", value_name="recovered")
-
-df_deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
-df_deaths = df_deaths[df_deaths["Country/Region"].isin(countries)]
-df_deaths_skimmed = df_deaths.drop(["Lat", "Long"], 1)
-df_deaths_rearranged = pd.melt(df_deaths_skimmed, id_vars=["Province/State", "Country/Region"], var_name="date", value_name="deaths")
-
-df_merged = df_confirmed_rearranged.merge(df_deaths_rearranged, how="outer").merge(df_recovered_rearranged, how="outer")
-df_merged = df_merged.groupby(["date", "Country/Region"]).sum().reset_index()
-df_merged["active"] = df_merged["confirmed"] - df_merged["recovered"] - df_merged["deaths"]
-df_merged["date"] = pd.to_datetime(df_merged["date"])
+df_merged = getJohnHopkinsCOVIDData(countries)
 
 df_countries = [df_merged[df_merged["Country/Region"] == country] for country in countries]
 
