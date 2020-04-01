39728a797c2c9fd11a6b77d10253025a90334e31
Simone Bologna <smnbologna@gmail.com>
2020-04-01 09:43:35 +0100

    Added SIR fit

---
+++ fitNationalSIR_New.py
@@ -0,0 +1,200 @@
+"""
+Andrea Dal Molin 09/03/2020
+Edited by Simone Bologna to fit more stuff
+This script applies the SIR epidemic model to the COVID19 epidemic
+https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology
+"""
+
+# ---------- Imports ----------
+import pandas as pd
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate, optimize
+import datetime as dt
+from JohnHopkinsDataProvider import getJohnHopkinsCOVIDData
+import matplotlib.dates as mdates
+
+class SIR:
+  def __init__(self, infected, removed, days, **kwargs):
+    self.infected = infected
+    self.removed = removed
+    self.days = days
+    
+
+  # ---------- Functions ----------
+  def sir_model(self, y, x, beta, gamma):
+      N, S, I, R  = y
+      dS = -beta * S * I / N
+      dR = gamma * I
+      dI = -(dS + dR)
+      return 0, dS, dI, dR
+
+
+  def fit_odeint(self, x, beta, gamma, N, S0, I0, R0):
+      return integrate.odeint(self.sir_model, (N, S0, I0, R0), x, args=(beta, gamma))[:,-2:].T.flatten()
+
+
+  def chisquare(self, comb_data, fitted):
+      return np.sum(((comb_data - fitted)**2)/comb_data)
+
+
+  def optimal_SIR(self, N):
+      # ---------- Initial parameters ----------
+      I0 = self.infected[0]  # Infected population
+      R0 = self.removed[0]   # Removed population (number of dead + number of recovered with immunity)
+      S0 = N - I0 - R0  # Susceptible population
+
+      # ---------- Fit parametes with current data ----------
+      comb_data = np.append(self.infected, self.removed)
+      fit =  lambda x, beta, gamma: self.fit_odeint(x, beta, gamma, N, S0, I0, R0)
+      popt, pcov = optimize.curve_fit(fit, self.days, comb_data, bounds=(0, 1))
+      fitted = self.fit_odeint(self.days, popt[0], popt[1], N, S0, I0, R0)
+      chi2 = self.chisquare(comb_data, fitted)
+      red_chi2 = chi2/(len(self.infected) + len(self.removed) - len(popt))
+      return red_chi2
+
+  def fit(self):
+    # ---------- Find optimal N parameter ----------
+
+    optimal_SIR =  lambda N: self.optimal_SIR(N)
+    result =optimize.minimize(optimal_SIR, x0, method = 'Nelder-Mead')
+    print(result)
+
+    # ---------- Initial parameters ----------
+    self.N = result.x      # Total population
+    self.I0 = self.infected[0]  # Infected population
+    self.R0 = self.removed[0]   # Removed population (number of dead + number of recovered with immunity)
+    self.S0 = self.N - self.I0 - self.R0  # Susceptible population
+
+    # ---------- Fit parametes with current data ----------
+    comb_data = np.append(self.infected, self.removed)
+    fit =  lambda x, beta, gamma: self.fit_odeint(x, beta, gamma, self.N, self.S0, self.I0, self.R0)
+    popt, pcov = optimize.curve_fit(fit, self.days, comb_data, bounds=(0, 1))
+    fitted = self.fit_odeint(self.days, popt[0], popt[1], self.N, self.S0, self.I0, self.R0)
+    chi2 = self.chisquare(comb_data, fitted)
+    red_chi2 = chi2/(len(self.infected) + len(self.removed) - len(popt))
+
+    # ---------- Compute BRN ----------
+    BRN = popt[0] / popt[1]  # Basic reproduction number
+    BRN_err = BRN * np.sqrt((np.sqrt(pcov[0][0])/popt[0])**2 + (np.sqrt(pcov[1][1])/popt[1])**2)
+
+    self.popt = popt
+    self.pcov = pcov
+    self.BRN = BRN
+    self.BRN_err = BRN
+    self.red_chi2 = red_chi2
+
+  def find_epidemic_peak(self, ndays):
+    # ---------- Extrapolate from model ----------
+    t =  np.arange(0,ndays)
+    model = integrate.odeint(self.sir_model, (self.N, self.S0, self.I0, self.R0), t, args=(self.popt[0], self.popt[1]))
+    self.N, self.S, self.I, self.R = model.T
+
+    # ---------- Find expected epidemic peak ----------
+    y_max = np.amax(self.I)
+    x_max = np.where(self.I == y_max)[0][0]
+    return x_max, y_max
+    
+
+# ---------- Load data from csv ----------
+
+# country = "United Kingdom" # 15/04
+country = "Switzerland" #04-05/04
+
+df = getJohnHopkinsCOVIDData([country])[0]
+# df = pd.concat([df[45:50], df[60:]])
+# df = df[50:]
+df = pd.concat([df[40:65], df[66:]])
+# df = df[52:]
+infected = np.array(df["active"])
+removed = np.array(df['recovered'] + df['deaths'])
+# import pdb ; pdb.set_trace()
+start_day = df.iloc[0]["date"]
+days = np.array((df["date"] - start_day)/np.timedelta64(1, "D"))
+nday_prediction = 60
+
+# infected[50:60] = 0
+# removed[50:60] = 0
+
+# infected = np.append(infected[30:50], infected[60:])
+# removed = np.append(removed[30:50], removed[60:])
+# days = np.append(days[30:50] - days[30], days[60:] - days[30])
+
+# infected = infected[40:]
+# removed = removed[40:]
+# days = days[40:] - days[40]
+x0 = np.array([50e3])
+
+sir = SIR(infected=infected, removed=removed, days=days)
+sir.fit()
+
+N = sir.N
+popt = sir.popt
+pcov = sir.pcov
+BRN = sir.BRN
+BRN_err = sir.BRN_err
+red_chi2 = sir.red_chi2
+
+textstr = '\n'.join((
+    r'MODEL PARAMETERS:  ',
+    r'$N=%.f$' % (N),
+    r'$\beta=%.3f \pm %.3f$' % (popt[0], np.sqrt(pcov[0][0])),
+    r'$\gamma=%.3f \pm %.3f$' % (popt[1], np.sqrt(pcov[1][1])),
+    r'$R_0=%.2f \pm %.2f$' %  (BRN, BRN_err),
+    r'$\chi_{red}^2=%.3f$' % (red_chi2)))
+
+
+x_max, y_max = sir.find_epidemic_peak(nday_prediction)
+
+peak_day = start_day + dt.timedelta(days = int(x_max)+1)
+peak_day_str = peak_day.strftime('%d-%m-%Y')
+
+# ---------- Plot results ----------
+fig = plt.figure(facecolor='w')
+ax = fig.add_subplot(111, axisbelow=True)
+
+
+# 
+# t = [start_day + dt.timedelta(days=ndays) for ndays in range(0, 140)]
+
+t = np.arange(start_day, start_day+dt.timedelta(days=nday_prediction), dtype='datetime64[D]')
+
+
+plt.xticks(rotation=90)
+ax.xaxis_date()
+ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=1))
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%d/%m"))
+# import pdb ; pdb.set_trace()
+
+ax.plot(df["date"], infected, 'ro', label='Data on positives')
+ax.plot(df["date"], removed, 'gx', label='Data on removed')
+ax.plot(t, sir.S, 'b', alpha=0.5, lw=2, label='Susceptible')
+# ax.plot(t, np.zeros(140), 'b', alpha=0.5, lw=2, label='Susceptible')
+ax.plot(t, sir.I, 'r', alpha=0.5, lw=2, label='Infected')
+# ax.plot(t, np.zeros(140), 'r', alpha=0.5, lw=2, label='Infected')
+ax.plot(t, sir.R, 'g', alpha=0.5, lw=2, label='Removed')
+# ax.plot(t, np.zeros(140), 'g', alpha=0.5, lw=2, label='Removed')
+
+ax.set_title('COVID-19 - ' + country)
+ax.set_xlabel('Number of days')
+ax.set_ylabel('Number of people')
+
+props = dict(boxstyle='round', edgecolor='0.7', facecolor='white', alpha=0.7)
+ax.text(0.676, 0.5, textstr, transform=ax.transAxes, fontsize=10,
+        verticalalignment='top', bbox=props)
+
+ax.annotate("EPIDEMIC PEAK\nInfected = " + str(int(y_max)) + "\non " + peak_day_str,
+            xy=(mdates.date2num(peak_day), y_max), xycoords='data',
+            xytext=(mdates.date2num(peak_day), y_max*1.2), textcoords='data',
+            arrowprops=dict(arrowstyle="->", connectionstyle="arc3"),
+            bbox=props
+            )
+
+ax.grid(b=True, which='major', ls='-')
+plt.legend(loc=1, bbox_to_anchor=(1, 0.9))
+
+# plt.tight_layout()
+# 
+plt.savefig("SIR_covid19_" + country.replace(" ", "") + ".png")
+plt.show()
